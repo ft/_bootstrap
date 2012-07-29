cd361818ede6585aae5702e451896fc998c35498
Frank Terbeck <ft@bewatermyfriend.org>
2012-07-29 11:39:31 +0200

    Update copyright years

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-## Copyright 2010
+## Copyright (c) 2010-2012
 ## Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
--- get_repos.pl
+++ get_repos.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-## Copyright 2010
+## Copyright (c) 2010-2012
 ## Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
