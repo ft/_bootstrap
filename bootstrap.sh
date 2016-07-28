6d8ac00eb875bd897ee17f86b1c76649c58c23c1
Frank Terbeck <ft@bewatermyfriend.org>
2016-07-29 01:50:00 +0200

    Update years and make → dewi
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-## Copyright (c) 2010-2012
+## Copyright (c) 2010-2016
 ## Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
--- get_repos.pl
+++ get_repos.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-## Copyright (c) 2010-2012
+## Copyright (c) 2010-2016
 ## Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
